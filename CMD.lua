0badc4b74bf831b92901cf0365ff7f49a72cf129
Atlas-Host <118232612+Atlas-Host@users.noreply.github.com>
2022-12-17 21:44:33 -0500

    Update CMD.lua

--- CMD.lua
+++ CMD.lua
@@ -1,3 +1,36 @@
 AddCommand("af2", {"antifling2"}, "Makes you unable to be flung in some cases ", function()
-print("hii")
+local lp = game:GetService("Players").LocalPlayer
+local rs = game:GetService("RunService")
+
+local Properties = PhysicalProperties.new(0, 0, 0, 0, 0)
+local Properties2 = PhysicalProperties.new(1/0, 1/0, 1/0, 1/0, 1/0)
+
+rs.Stepped:Connect(function()
+    for i,v in pairs(lp.Parent:GetPlayers()) do
+        if v.Name ~= lp.Name then
+            pcall(function()
+                if v.Character then
+                    for j, x in pairs(v.Character:GetDescendants()) do
+                        if x.ClassName:lower():find("part") then
+                            pcall(function()
+                                x.CanCollide = false
+                                x.Velocity = Vector3.new()
+                                x.RotVelocity = Vector3.new()
+                                x.CustomPhysicalProperties = Properties
+                                x.CanCollide = false
+                            end) 
+                        end
+                    end
+                end
+            end) 
+        end
+    end
+    pcall(function()
+        for i,v in pairs(lp.Character:GetDescendants()) do
+            if v.ClassName:lower():find("part") then
+                v.CustomPhysicalProperties = Properties2
+            end
+        end
+    end)
+end)
 end)
